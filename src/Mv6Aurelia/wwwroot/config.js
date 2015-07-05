0f492ed4b909872cbde9ab93e6e8a5b5ea5404b1
Peter Morlion <peter.morlion@live.be>
2015-07-05 13:52:21 +0200

    jspm installed aurelia framework

--- src/Mv6Aurelia/wwwroot/config.js
+++ src/Mv6Aurelia/wwwroot/config.js
@@ -16,9 +16,52 @@ System.config({
 
 System.config({
   "map": {
+    "aurelia-framework": "github:aurelia/framework@0.13.1",
     "babel": "npm:babel-core@5.6.15",
     "babel-runtime": "npm:babel-runtime@5.6.15",
     "core-js": "npm:core-js@0.9.18",
+    "github:aurelia/binding@0.8.0": {
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.6.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/dependency-injection@0.9.0": {
+      "aurelia-logging": "github:aurelia/logging@0.6.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/framework@0.13.1": {
+      "aurelia-binding": "github:aurelia/binding@0.8.0",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.0",
+      "aurelia-loader": "github:aurelia/loader@0.8.0",
+      "aurelia-logging": "github:aurelia/logging@0.6.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+      "aurelia-path": "github:aurelia/path@0.8.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.6.0",
+      "aurelia-templating": "github:aurelia/templating@0.13.1",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/loader@0.8.0": {
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
+      "aurelia-path": "github:aurelia/path@0.8.0",
+      "core-js": "npm:core-js@0.9.18",
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
+    },
+    "github:aurelia/metadata@0.7.0": {
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/templating@0.13.1": {
+      "aurelia-binding": "github:aurelia/binding@0.8.0",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.0",
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
+      "aurelia-loader": "github:aurelia/loader@0.8.0",
+      "aurelia-logging": "github:aurelia/logging@0.6.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+      "aurelia-path": "github:aurelia/path@0.8.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.6.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
