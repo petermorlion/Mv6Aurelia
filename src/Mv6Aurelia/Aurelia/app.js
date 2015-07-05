02a7cff35b0e80134f3bd662d21573ae2fce0b2d
Peter Morlion <peter.morlion@live.be>
2015-07-05 15:12:47 +0200

    remove bootstrap from app.js

--- src/Mv6Aurelia/Aurelia/app.js
+++ src/Mv6Aurelia/Aurelia/app.js
@@ -1,7 +1,4 @@
-﻿import 'bootstrap';
-import 'bootstrap/css/bootstrap.css!';
-
-export class App {
+﻿export class App {
     configureRouter(config, router){
         config.map([
           { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
