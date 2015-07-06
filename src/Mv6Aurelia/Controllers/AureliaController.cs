cde96b42dd4cbf14b7e65a581f49be50101ffa83
Peter Morlion <peter.morlion@live.be>
2015-07-06 21:28:54 +0200

    Add server-side part of template

--- src/Mv6Aurelia/Controllers/AureliaController.cs
+++ src/Mv6Aurelia/Controllers/AureliaController.cs
@@ -8,12 +8,31 @@ using Microsoft.AspNet.Mvc;
 
 namespace Mv6Aurelia.Controllers
 {
+    public class DynamicTemplateViewModel
+    {
+        public string ServerPath { get; set; }
+        public string Property { get; set; }
+    }
+
     public class AureliaController : Controller
     {
         // GET: /<controller>/
         public IActionResult DynamicTemplate()
         {
-            return View();
+            var model = new DynamicTemplateViewModel
+            {
+#if DNX451
+                ServerPath = AppDomain.CurrentDomain.BaseDirectory,
+                Property = "AppDomain.CurrentDomain.BaseDirectory"
+#endif
+#if DNXCORE50
+                ServerPath = AppContext.BaseDirectory,
+                Property = "AppContext.BaseDirectory"
+#endif
+
+            };
+
+            return View(model);
         }
     }
 }
