ba815389e0fe9edcb1f5d3e3c1e3f39e8612a0fd
Peter Morlion <peter.morlion@live.be>
2015-07-06 20:49:13 +0200

    removed welcome

--- src/Mv6Aurelia/Aurelia/app.js
+++ src/Mv6Aurelia/Aurelia/app.js
@@ -19,8 +19,7 @@ ConventionalViewStrategy.convertModuleIdToViewUrl = function(moduleId){
 export class App {
     configureRouter(config, router){
         config.map([
-          { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
-          { route: 'static-template', name: 'static-template', moduleId: './static-template', nav: true, title:'Static Template' },
+          { route: ['','static-template'], name: 'static-template', moduleId: './static-template', nav: true, title:'Static Template' },
           { route: 'dynamic-template', name: 'dynamic-template', moduleId: './dynamic-template', nav: true, title:'Dynamic Template' },
         ]);
 
